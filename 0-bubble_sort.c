0c064df6b8520b0a3518a330ad7535849bfb4017
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 <11534@holbertonstudents.com>
2025-08-06 15:29:19 +0200

    fix function bubble_sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,6 +1,42 @@
+#include <stdio.h>
 #include "sort.h"
 
+/**
+ * switch_sort - function to switch 2 elements of the array
+ * @array: pointer to the first index of the array
+ * @i: first elemment
+ * @j: second element
+ * Return: the array with the elements switched
+ */
+int *switch_sort(int *array, size_t i, size_t j)
+{
+	size_t temp;
+
+	temp = array[i];
+	array[i] = array[j];
+	array[j] = temp;
+
+	return (array);
+}
+
+/**
+ * bubble_sort - function to sort with the bubble method
+ * @array: pointer to the first index of the array
+ * @size: size of the array
+ */
 void bubble_sort(int *array, size_t size)
 {
+	size_t i, j;
 
+	if (array == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+				switch_sort(array, j, j + 1);
+		}
+	}
 }
