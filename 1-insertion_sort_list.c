2a59ca9d26ef888085524e4752fb752ae22d23e7
arsicbt <chobertarsinoe@gmail.com>
2025-08-07 12:37:31 +0000

    fix: Add a switch function, change the logic

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,21 +1,55 @@
 #include <stdio.h>
 #include "sort.h"
 
+/**
+ * switch_sort - change 2 nodes in a doubly linked list
+ * @list: pointer to the start of the list
+ * @node1: pointer to the first node to change.
+**/
+void switch_sort_insert(listint_t **list, listint_t *node1)
+{
+	listint_t *node2 = node1->next;
+
+	if (!node1 || !node2)
+		return;
+
+	if (node1->prev)
+		node1->prev->next = node2;
+	else
+		*list = node2;
+
+	if (node2->next)
+		node2->next->prev = node1;
+
+	node1->next = node2->next;
+	node2->prev = node1->prev;
+	node2->next = node1;
+	node1->prev = node2;
+}
+
+/**
+**/
 void insertion_sort_list(listint_t **list)
 {
-	int i, j, insert_value;
+	listint_t *i, *insert_node;
+	int insert_value;
 
 	if (list == NULL)
 		return;
 
-	for (i = 0; i < list -1; i++)
+	i = (*list)->next;
+
+	while (i != NULL)
 	{
-		insert_value = (*list)->n;
-		for (j = i; j < 0 && insert_value < (*list)->prev->n; j--)
+		insert_node = i;
+		insert_value = i->n;
+
+		while (insert_node->prev && insert_value < insert_node->prev->n)
 		{
-			(*list)->n = (*list)->prev->n;
+			switch_sort_insert(list, insert_node->prev);
+			print_list(*list);
 		}
-		list->n = insert_value;
-		print_list(*list);
+
+		i = i->next;
 	}
 }
