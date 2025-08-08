23711fbc8de4edd74a2d1ea61f68a9c3b6c2c7da
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 <11534@holbertonstudents.com>
2025-08-08 16:45:21 +0200

    add function quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -8,5 +8,30 @@
  */
 void quick_sort(int *array, size_t size)
 {
-	
+    size_t i, j;
+	int index = 0, temp = 0, changement = 0;
+
+	if (array == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		changement = 0;
+		temp = array[i];
+		for (j = i + 1; j < size; j++)
+		{
+			if (temp > array[j])
+			{
+				temp = array[j];
+				index = j;
+				changement = 1;
+			}
+		}
+		if (changement == 1)
+		{
+			array[index] = array[i];
+			array[i] = temp;
+			print_array(array, size);
+		}
+	}
 }
