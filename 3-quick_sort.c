9cf1131dd776a80f8dd097a18203b1e86443362c
arsicbt <chobertarsinoe@gmail.com>
2025-08-08 17:09:11 +0000

    fix: Application of the structure of the quickSort algorithm

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -2,36 +2,87 @@
 #include "sort.h"
 
 /**
- * quick_sort - sort with quick sort
- * @array: pointer to the first index of the array
- * @size: size of the array
+ * swap - Swap two integers in an array.
+ * @x: First integer to swap.
+ * @y: Second integer to swap.
+ *
+ * Picture two kids trading Pok  mon cards  ^`^t they just swap them.
+ * This function does exactly that but with numbers inside memory.
+**/
+void swap(int *x, int *y)
+{
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
+/**
+ * lomuto - Lomuto partition scheme for quicksort.
+ * @array: The array of integers to sort.
+ * @low: Starting index of the partition.
+ * @high: Ending index of the partition.
+ * @size: Size of the full array.
+ *
+ * This is where the magic happens  ^`^t we pick the last element as
+ * the 'pivot' and rearrange the array so that:
+ * all numbers smaller than or equal to it are on the left,
+ * and all the bigger numbers go on the right.
+**/
+int lomuto(int *array, int low, int high)
+{
+	int pivot_value = array[high];
+	int i, j;
+
+	i = low;
+
+	for (j = low; j < high; j++)
+	{
+		if (array[j] <= pivot_value)
+		{
+			swap(&array[i], &array[j]);
+			i++;
+		}
+	}
+	swap(&array[i], &array[high]);
+	return (i);
+}
+
+/**
+ * quick_sort - Sort an array of integers in ascending order using quicksort.
+ * @array: The array to sort.
+ * @size: The size of the array.
+ *
+ * This is the 'party planner' - it sets everything up for quicksort
+ * by calling the recursive function with the full array.
+ * If the array is empty or has only one element, we just... do nothing.
  */
 void quick_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int index = 0, temp = 0, changement = 0;
+	quicksort_recursion(array, 0, size -1, size);
+}
 
-	if (array == NULL)
-		return;
+/**
+ * quicksort_recursion - Recursively sort an array using quicksort.
+ * @array: The array of integers to sort.
+ * @low: Starting index of the current partition.
+ * @high: Ending index of the current partition.
+ * @size: Size of the full array.
+ *
+ * Think of this like cutting a cake into smaller and smaller pieces
+ * until all slices are perfectly sized. We use recursion to zoom into
+ * each partition until it's so small (one element) that it's already 'sorted'.
+**/
+void quicksort_recursion(int *array, int low, int high, size_t size)
+{
+	int pivot_index;
 
-	for (i = 0; i < size; i++)
+	if (low < high)
 	{
-		changement = 0;
-		temp = array[i];
-		for (j = i + 1; j < size; j++)
-		{
-			if (temp > array[j])
-			{
-				temp = array[j];
-				index = j;
-				changement = 1;
-			}
-		}
-		if (changement == 1)
-		{
-			array[index] = array[i];
-			array[i] = temp;
-			print_array(array, size);
-		}
+		pivot_index = lomuto(array, low, high);
+
+		print_array(array, size);
+
+		quicksort_recursion(array, low, pivot_index -1, size);
+		quicksort_recursion(array, pivot_index +1, high, size);
 	}
 }
